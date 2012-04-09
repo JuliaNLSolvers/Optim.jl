fe8e0e695a863756e9d421f29ffbc19d78262a83
John Myles White <jmw@johnmyleswhite.com>
2012-04-09 14:04:10 -0400

    Confirmed Nelder-Mead works on Rosenbrock function

--- tests/nelder_mead.jl
+++ tests/nelder_mead.jl
@@ -25,3 +25,17 @@ max_iterations = 100
 solution = nelder_mead(f, initial_p, a, g, b, tolerance, max_iterations, true)
 
 @assert norm(solution[1] - [0 0]) < 0.01
+
+function rosenbrock(x, y)
+  (1 - x)^2 + 100(y - x^2)^2
+end
+
+initial_p = [-10. -15.; 5. 1.; 1. 17.;]
+
+tolerance = 10e-8
+
+max_iterations = 1000
+
+solution = nelder_mead(x -> rosenbrock(x[1, 1], x[1, 2]), initial_p, a, g, b, tolerance, max_iterations, false)
+
+@assert norm(solution[1] - [1 1]) < 0.01
