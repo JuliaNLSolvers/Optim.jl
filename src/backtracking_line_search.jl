b0ae1e4e827751d1fd7394b231b3b71bf45c52e7
John Myles White <jmw@johnmyleswhite.com>
2012-06-30 19:32:19 -0700

    Very rough draft of BFGS

--- src/backtracking_line_search.jl
+++ src/backtracking_line_search.jl
@@ -4,7 +4,7 @@ function backtracking_line_search(f, g, x, dx, alpha, beta)
   
   t = 1
   
-  while any(f(x + t * dx) > f(x) + alpha * g(x)' * dx)
+  while any(f(x + t * dx) .> f(x) + alpha * g(x)' * dx)
     t = beta * t
     
     i = i + 1
---
+++ src/bfgs.jl
@@ -0,0 +1,50 @@
+function update_h(h, s, y)
+  rho = 1 / (y' * s)[1]
+  I = eye(size(h, 1))
+  (I - rho * s * y') * h * (I - rho * y * s') + rho * s * s'
+end
+
+function bfgs(f, g, initial_x, initial_h, tolerance)
+  k = 0
+  
+  x_new = initial_x
+  x_old = initial_x
+  
+  gradient_new = g(x_new)
+  gradient_old = g(x_old)
+  
+  h = initial_h
+  
+  max_iterations = 1000
+  
+  a = 0.1
+  b = 0.8
+  
+  while norm(gradient_new) > tolerance && k <= max_iterations
+    p = -h * gradient_new
+    alpha = backtracking_line_search(f, g, x_new, p, a, b)
+    x_old = x_new
+    x_new = x_old + alpha * p
+    s = x_new - x_old
+    gradient_old = gradient_new
+    gradient_new = g(x_new)
+    y = gradient_new - gradient_old
+    h = update_h(h, s, y)
+    k = k + 1
+    println(k)
+    println(x_new)
+    println(f(x_new))
+    println()
+  end
+end
+
+f = function(x) x[1]^2 + 2x[2]^2 end
+function g(x)
+  [2x[1], 4x[2]]
+end
+
+initial_x = [100.0, 100.0]
+initial_h = eye(2)
+tolerance = 10e-8
+
+bfgs(f, g, initial_x, initial_h, tolerance)
