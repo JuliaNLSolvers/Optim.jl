30b875a90c3903d269ddf051e1e3dc515b10ad0a
John Myles White <jmw@johnmyleswhite.com>
2012-07-03 08:52:34 -0700

    Added nicely formated summary

--- src/types.jl
+++ src/types.jl
@@ -6,3 +6,19 @@ type OptimizationResults
   iterations::Int64
   converged::Bool
 end
+
+function print(results::OptimizationResults)
+  println()
+  println("Results of Optimization Algorithm")
+  println(" * Algorithm: $(results.method)")
+  println(" * Starting Point: $(results.initial_x)")
+  println(" * Minimum: $(results.minimum)")
+  println(" * Value of Function at Minimum: $(results.f_minimum)")
+  println(" * Iterations: $(results.iterations)")
+  println(" * Self-Reported Convergence: $(results.converged)")
+  println()
+end
+
+function show(results::OptimizationResults)
+  print(results)
+end
