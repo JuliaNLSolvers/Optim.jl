d389f8d3e2c829dafbb39b575b52004d70ec9af7
Miles Lubin <miles.lubin@gmail.com>
2013-10-23 17:23:42 -0400

    travis/testing fixes

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,20 +4,20 @@
 
 using Optim
 
-my_tests = ["test/bfgs.jl",
-            "test/curve_fit.jl",
-            "test/gradient_descent.jl",
-            "test/momentum_gradient_descent.jl",
-            "test/grid_search.jl",
-            "test/l_bfgs.jl",
-            "test/levenberg_marquardt.jl",
-            "test/newton.jl",
-            "test/cg.jl",
-            "test/nelder_mead.jl",
-            "test/optimize.jl",
-            "test/simulated_annealing.jl",
-            "test/interpolating_line_search.jl",
-            "test/api.jl"]
+my_tests = ["bfgs.jl",
+            "curve_fit.jl",
+            "gradient_descent.jl",
+            "momentum_gradient_descent.jl",
+            "grid_search.jl",
+            "l_bfgs.jl",
+            "levenberg_marquardt.jl",
+            "newton.jl",
+            "cg.jl",
+            "nelder_mead.jl",
+            "optimize.jl",
+            "simulated_annealing.jl",
+            "interpolating_line_search.jl",
+            "api.jl"]
 
 println("Running tests:")
 
