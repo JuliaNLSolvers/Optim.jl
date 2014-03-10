3131af4b62f8f8fab3a5684a88dde6498a285caa
timholy <tim.holy@gmail.com>
2014-03-10 16:15:22 -0500

    Improve stability of hz_linesearch!
    - For non-finite values, update alphamax and use more bisection (this
      seems to be a significant improvement when it comes to handling
      barriers)
    - Use nextfloat in tests for convergence (this avoids trouble when
      you're right at the optimum)
    - Make hz_linesearch! more robust against floating-point oddities

--- src/utilities/assess_convergence.jl
+++ src/utilities/assess_convergence.jl
@@ -15,7 +15,7 @@ function assess_convergence(x::Array,
     # Absolute Tolerance
     # if abs(f_x - f_x_previous) < ftol
     # Relative Tolerance
-    if abs(f_x - f_x_previous) / (abs(f_x) + ftol) < ftol
+    if abs(f_x - f_x_previous) / (abs(f_x) + ftol) < ftol || nextfloat(f_x) >= f_x_previous
         f_converged = true
     end
 
