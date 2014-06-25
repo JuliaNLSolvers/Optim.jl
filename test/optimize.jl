464ad55792c80bb2ea695990a079bc1e46f93d26
Thomas R. Covert <thomc@alum.mit.edu>
2014-06-25 14:54:11 -0500

    added a test for bfgs_initial_invH

--- test/optimize.jl
+++ test/optimize.jl
@@ -32,3 +32,11 @@ results = optimize(f1, [127.0, 921.0], autodiff = true)
 @assert results.f_converged
 @assert norm(results.minimum - [0.0, 0.0]) < 0.01
 
+# tests for bfgs_initial_invH
+initial_invH = zeros(2,2)
+h1([127.0, 921.0],initial_invH)
+initial_invH = diagm(diag(initial_invH))
+results = optimize(f1, g1, [127.0, 921.0], 
+                   method = :bfgs, bfgs_initial_invH = initial_invH)
+@assert results.gr_converged
+@assert norm(results.minimum - [0.0, 0.0]) < 0.01
