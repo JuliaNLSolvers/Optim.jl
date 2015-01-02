14b1f16f9127551b96f2975d54c84890a0c016b6
Dan Kolbman <dankolbman@gmail.com>
2015-01-01 19:40:17 -0500

    Fix deprecated functions/syntax for Julia 0.4, add Compat for backward compatability

--- src/nnls.jl
+++ src/nnls.jl
@@ -6,7 +6,7 @@ function nnls(A::AbstractMatrix, b::AbstractVector)
     x = fill(one(T), size(A, 2))
     # Set up constraints
     l = zeros(eltype(x), length(x))
-    u = fill(inf(eltype(x)), length(x))
+    u = fill(convert(eltype(x), Inf), length(x))
     # Perform the optimization    
     func = (x, g) -> nnlsobjective(x, g, A, b)
     df = DifferentiableFunction(x->func(x,nothing), func, func)
