287651ec1d5c39dd9d40c3501f5fab41351c9fec
Patrick Kofod Mogensen <patrick.kofod.mogensen@econ.ku.dk>
2015-03-04 16:28:18 +0100

    Added problem hessians and tests
    permission change
    
    permission change
    
    Changed space around ^

--- test/nelder_mead.jl
+++ test/nelder_mead.jl
@@ -1,3 +1,14 @@
+using Optim
+
+# Test Optim.nelder_mead for all functions except Large Polynomials in Optim.UnconstrainedProblems.examples
+for (name, prob) in Optim.UnconstrainedProblems.examples
+	f_prob = prob.f
+	res = Optim.nelder_mead(f_prob, prob.initial_x)
+	if name != "Large Polynomial" # FIX THIS!
+		@assert norm(res.minimum - prob.solutions) < 1e-2
+	end
+end
+
 function f(x::Vector)
   (100.0 - x[1])^2 + x[2]^2
 end
@@ -19,3 +30,5 @@ results = Optim.nelder_mead(rosenbrock, initial_x)
 @assert results.f_converged
 @assert norm(results.minimum - [1.0, 1.0]) < 0.01
 @assert length(results.trace.states) == 0
+
+
