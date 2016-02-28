543d7b7d37f0c5e94d31d0c212a5cd8a80a17346
Michael Creel <michael.creel@uab.es>
2016-02-28 18:09:28 +0100

    this version has tabs converted to 4 spaces

--- src/samin.jl
+++ src/samin.jl
@@ -11,8 +11,8 @@
   
   The original Fortran code accompanies  the article
   Goffe, William L. (1996) "SIMANN: A Global Optimization Algorithm
-	using Simulated Annealing " Studies in Nonlinear Dynamics & Econometrics
-  	Oct96, Vol. 1 Issue 3.
+    using Simulated Annealing " Studies in Nonlinear Dynamics & Econometrics
+    Oct96, Vol. 1 Issue 3.
 
   This Julia code uses the same names for control variables,
   for the most part. A notable difference is that the initial
@@ -25,12 +25,12 @@
 
   Other relevant articles (among many):
   Corana et. al., (1987) "Minimizing Multimodal Functions of Continuous
-	Variables with the "Simulated Annealing" Algorithm",
- 	ACM Transactions on Mathematical Software, V. 13, N. 3.
+    Variables with the "Simulated Annealing" Algorithm",
+    ACM Transactions on Mathematical Software, V. 13, N. 3.
 
   Goffe, et. al. (1994) "Global Optimization of Statistical Functions
- 	with Simulated Annealing", Journal of Econometrics,
- 	V. 60, N. 1/2.    
+    with Simulated Annealing", Journal of Econometrics,
+    V. 60, N. 1/2.    
 
 usage: x, obj, convergence, details = samin(f,
                                             x_init,
@@ -60,9 +60,9 @@ KEYWORDS
                    comparisons
 * paramtol: (> 0): (default = 1e-5) the required tolerance level for parameters
 * verbosity: scalar: 0, 1, 2 or 3 (default = 1).
-	* 0 = no screen output
-	* 1 = only final results to screen
-	* 2 = summary every temperature change, without param values
+    * 0 = no screen output
+    * 1 = only final results to screen
+    * 2 = summary every temperature change, without param values
     * 3 = summary every temperature change, with param values
 * coverage_ok: (0 or 1) (default = 0) 0: increase temperature until parameter space is
         covered by the trial values. 1: start decreasing temperature immediately
@@ -70,10 +70,10 @@ Returns:
 * x: the minimizer
 * obj: the value of f() at x
 * convergence:
-	0 if no convergence within maxevals function evaluations
-	1 if normal convergence to a point interior to the parameter space
-	2 if convergence to point very near bounds of parameter space
-	  (suggest re-running with looser bounds)
+    0 if no convergence within maxevals function evaluations
+    1 if normal convergence to a point interior to the parameter space
+    2 if convergence to point very near bounds of parameter space
+      (suggest re-running with looser bounds)
 * details: a px3 matrix. p is the number of times improvements were found.
            The columns record information at the time an improvement was found
            * first: cumulative number of function evaluations
@@ -85,139 +85,139 @@ Example: see samin_test
 function samin(obj_fn, x, lb, ub; nt=5, ns=5, rt=0.5, maxevals=1e6, neps=5, functol=1e-8, paramtol=1e-5, verbosity=1, coverage_ok=0)
     n = size(x,1) # dimension of parameter
     #  Set initial values
-	nacc = 0 # total accepted trials
-	t = 2.0 # temperature - will initially rise or fall to cover parameter space. Then it will fall
-	converge = 0 # convergence indicator 0 (failure), 1 (normal success), or 2 (convergence but near bounds)
-	# most recent values, to compare to when checking convergend
-	fstar = typemax(Float64)*ones(neps)
-	# Initial obj_value
-	xopt = copy(x)
+    nacc = 0 # total accepted trials
+    t = 2.0 # temperature - will initially rise or fall to cover parameter space. Then it will fall
+    converge = 0 # convergence indicator 0 (failure), 1 (normal success), or 2 (convergence but near bounds)
+    # most recent values, to compare to when checking convergend
+    fstar = typemax(Float64)*ones(neps)
+    # Initial obj_value
+    xopt = copy(x)
     f = obj_fn(x)
-	fopt = copy(f) # give it something to compare to
-	func_evals = 0 # total function evaluations (limited by maxeval)
-	details = [func_evals t fopt]
+    fopt = copy(f) # give it something to compare to
+    func_evals = 0 # total function evaluations (limited by maxeval)
+    details = [func_evals t fopt]
     bounds = ub - lb
-	# check for out-of-bounds starting values
-	for i = 1:n
-		if(( x[i] > ub[i]) || (x[i] < lb[i]))
+    # check for out-of-bounds starting values
+    for i = 1:n
+        if(( x[i] > ub[i]) || (x[i] < lb[i]))
             @printf("samin: initial parameter %d out of bounds\n", i)
             converge = 0
-			return xopt, fopt, converge, details
-		end	
+            return xopt, fopt, converge, details
+        end 
     end
-	# main loop, first increase temperature until parameter space covered, then reduce until convergence
-	while (converge==0)
-		# statistics to report at each temp change, set back to zero
-		nup = 0
-		nrej = 0
-		nnew = 0
-		ndown = 0
-		lnobds = 0
+    # main loop, first increase temperature until parameter space covered, then reduce until convergence
+    while (converge==0)
+        # statistics to report at each temp change, set back to zero
+        nup = 0
+        nrej = 0
+        nnew = 0
+        ndown = 0
+        lnobds = 0
 
-		# repeat nt times then adjust temperature
-		for m = 1:nt
-			# repeat ns times, then adjust bounds
+        # repeat nt times then adjust temperature
+        for m = 1:nt
+            # repeat ns times, then adjust bounds
             nacp = zeros(n)
-			for j = 1:ns
-				# generate new point by taking last and adding a random value
-				# to each of elements, in turn
-				for h = 1:n
-					# new Sept 2011, if bounds are same, skip the search for that vbl.
+            for j = 1:ns
+                # generate new point by taking last and adding a random value
+                # to each of elements, in turn
+                for h = 1:n
+                    # new Sept 2011, if bounds are same, skip the search for that vbl.
                     # Allows restrictions without complicated programming
-					if (lb[h] != ub[h])
-						xp = copy(x)
-						xp[h] += (2.0 * rand() - 1.0) * bounds[h]
-						if((xp[h] < lb[h]) || (xp[h] > ub[h]))
-							xp[h] = lb[h] + (ub[h] - lb[h]) * rand()
-							lnobds += 1
+                    if (lb[h] != ub[h])
+                        xp = copy(x)
+                        xp[h] += (2.0 * rand() - 1.0) * bounds[h]
+                        if((xp[h] < lb[h]) || (xp[h] > ub[h]))
+                            xp[h] = lb[h] + (ub[h] - lb[h]) * rand()
+                            lnobds += 1
                         end
-						# Evaluate function at new point
-						fp = obj_fn(xp)
-						func_evals += 1
-						#  Accept the new point if the function value decreases
-						if (fp <= f)
-							x = copy(xp)
-							f = copy(fp)
-							nacc += 1 # total number of acceptances
-							nacp[h] += 1 # acceptances for this parameter
-							nup += 1
-							#  If lower than any other point, record as new optimum
-							if(fp < fopt)
-								xopt = copy(xp)
-								fopt = copy(fp)
-								nnew +=1
-								details = [details; [func_evals t fp]]
+                        # Evaluate function at new point
+                        fp = obj_fn(xp)
+                        func_evals += 1
+                        #  Accept the new point if the function value decreases
+                        if (fp <= f)
+                            x = copy(xp)
+                            f = copy(fp)
+                            nacc += 1 # total number of acceptances
+                            nacp[h] += 1 # acceptances for this parameter
+                            nup += 1
+                            #  If lower than any other point, record as new optimum
+                            if(fp < fopt)
+                                xopt = copy(xp)
+                                fopt = copy(fp)
+                                nnew +=1
+                                details = [details; [func_evals t fp]]
                             end
-						# If the point is higher, use the Metropolis criteria to decide on
-						# acceptance or rejection.
-						else
-							p = exp(-(fp - f) / t)
-							if(rand() < p)
-								x = copy(xp)
-								f = copy(fp)
-								nacc += 1
-								nacp[h] += 1
-								ndown += 1
-							else
+                        # If the point is higher, use the Metropolis criteria to decide on
+                        # acceptance or rejection.
+                        else
+                            p = exp(-(fp - f) / t)
+                            if(rand() < p)
+                                x = copy(xp)
+                                f = copy(fp)
+                                nacc += 1
+                                nacp[h] += 1
+                                ndown += 1
+                            else
                                 nrej += 1
                             end
                         end
-					    # If maxevals exceeded, terminate the algorithm
-					    if(func_evals >= maxevals)
-						    if(verbosity >= 1)
-							    println("================================================")
-					            println("SAMIN results")
-							    println("NO CONVERGENCE: MAXEVALS exceeded")
-					            @printf("\n     Obj. value:  %16.5f\n\n", fopt)
-        			            println("       parameter      search width")
+                        # If maxevals exceeded, terminate the algorithm
+                        if(func_evals >= maxevals)
+                            if(verbosity >= 1)
+                                println("================================================")
+                                println("SAMIN results")
+                                println("NO CONVERGENCE: MAXEVALS exceeded")
+                                @printf("\n     Obj. value:  %16.5f\n\n", fopt)
+                                println("       parameter      search width")
                                 if(verbosity > 2)
                                     for i=1:n
                                         @printf("%16.5f  %16.5f \n", xopt[i], bounds[i])
                                     end
                                 end
-					            println("================================================")
+                                println("================================================")
                             end
                             converge = 0
-						    return xopt, fopt, converge, details
+                            return xopt, fopt, converge, details
                         end
                     end
                 end
             end
             #  Adjust bounds so that approximately half of all evaluations are accepted
-			test = 0
-			for i = 1:n
-				if (lb[i] != ub[i])
-					ratio = nacp[i] / ns
+            test = 0
+            for i = 1:n
+                if (lb[i] != ub[i])
+                    ratio = nacp[i] / ns
                     if(ratio > 0.6) bounds[i] = bounds[i] * (1.0 + 2.0 * (ratio - 0.6) / 0.4) end
                     if(ratio < .4) bounds[i] = bounds[i] / (1.0 + 2.0 * ((0.4 - ratio) / 0.4)) end
                     # keep within initial bounds
-					if(bounds[i] >= (ub[i] - lb[i]))
-						bounds[i] = ub[i] - lb[i]
-						test += 1
+                    if(bounds[i] >= (ub[i] - lb[i]))
+                        bounds[i] = ub[i] - lb[i]
+                        test += 1
                     end
-				else
+                else
                     test = test + 1 # make sure coverage check passes for the fixed parameters
                 end    
             end
             nacp = nacp - nacp # set back to zero
-			# check if we cover parameter space, if we have yet to do so
+            # check if we cover parameter space, if we have yet to do so
             if (coverage_ok != 1) coverage_ok = (test == n) end
         end
 
         # intermediate output, if desired
-		if(verbosity >= 2)
-			println("samin: intermediate results before next temperature change")
-			println("temperature: ", t)
-			println("current best function value: ", fopt)
-			println("total evaluations so far: ", func_evals)
-			println("total moves since last temperature reduction: ", nup + ndown + nrej)
-			println("downhill: ", nup)
-			println("accepted uphill: ", ndown)
-			println("rejected uphill: ", nrej)
-			println("out of bounds trials: ", lnobds)
-			println("new minima this temperature: ", nnew)
+        if(verbosity >= 2)
+            println("samin: intermediate results before next temperature change")
+            println("temperature: ", t)
+            println("current best function value: ", fopt)
+            println("total evaluations so far: ", func_evals)
+            println("total moves since last temperature reduction: ", nup + ndown + nrej)
+            println("downhill: ", nup)
+            println("accepted uphill: ", ndown)
+            println("rejected uphill: ", nrej)
+            println("out of bounds trials: ", lnobds)
+            println("new minima this temperature: ", nnew)
             println()
-			println("       parameter      search width")
+            println("       parameter      search width")
             if (verbosity > 2)
                 for i=1:n
                     @printf("%16.5f  %16.5f \n", xopt[i], bounds[i])
@@ -226,24 +226,24 @@ function samin(obj_fn, x, lb, ub; nt=5, ns=5, rt=0.5, maxevals=1e6, neps=5, func
             println()
         end        
         # Check for convergence, if we have covered the parameter space
-		if (coverage_ok)
+        if (coverage_ok)
 
             # last value close enough to last neps values?
-			fstar[1] = f
-			test = 0
+            fstar[1] = f
+            test = 0
             for i=1:neps
                 test += (abs(f - fstar[i]) > functol)
             end
-			test = (test > 0) # if different from zero, function conv. has failed
+            test = (test > 0) # if different from zero, function conv. has failed
 
-			# last value close enough to overall best?
-			if (((fopt - f) <= functol) && (!test))
-				# check for bound narrow enough for parameter convergence
-				for i = 1:n
-					if (bounds[i] > paramtol)
-						converge = 0 # no conv. if bounds too wide
-						break
-					else
+            # last value close enough to overall best?
+            if (((fopt - f) <= functol) && (!test))
+                # check for bound narrow enough for parameter convergence
+                for i = 1:n
+                    if (bounds[i] > paramtol)
+                        converge = 0 # no conv. if bounds too wide
+                        break
+                    else
                         converge = 1
                     end    
                 end
@@ -253,42 +253,42 @@ function samin(obj_fn, x, lb, ub; nt=5, ns=5, rt=0.5, maxevals=1e6, neps=5, func
                 converge = 2
             end
             # Like to see the final results?
-			if (converge > 0)
-				if (verbosity >= 1)
-					println("================================================")
-					println("SAMIN results")
+            if (converge > 0)
+                if (verbosity >= 1)
+                    println("================================================")
+                    println("SAMIN results")
                     if (converge == 1)
                         println("==> Normal convergence <==")
                     end
-					if (converge == 2)
-						println("==> WARNING <==: Last point satisfies convergence criteria,")
-						println("but is near boundary of parameter space.")
-						println(lnobds, " out of  ", (nup+ndown+nrej), " evaluations were out of bounds in the last round.")
-						println("Expand bounds and re-run, unless this is a constrained minimization.")
+                    if (converge == 2)
+                        println("==> WARNING <==: Last point satisfies convergence criteria,")
+                        println("but is near boundary of parameter space.")
+                        println(lnobds, " out of  ", (nup+ndown+nrej), " evaluations were out of bounds in the last round.")
+                        println("Expand bounds and re-run, unless this is a constrained minimization.")
                     end
-					@printf("\n     Obj. value:  %16.5f\n\n", fopt)
-        			println("       parameter      search width")
+                    @printf("\n     Obj. value:  %16.5f\n\n", fopt)
+                    println("       parameter      search width")
                     for i=1:n
                         @printf("%16.5f  %16.5f \n", xopt[i], bounds[i])
                     end
-					println("================================================")
+                    println("================================================")
                 end
             end
             # Reduce temperature, record current function value in the
-			# list of last "neps" values, and loop again
-			t = rt * t
+            # list of last "neps" values, and loop again
+            t = rt * t
             for i = neps:-1:2
                 fstar[i] = fstar[i-1]
             end    
-			f = copy(fopt)
-			x = copy(xopt)
-		else  # coverage not ok - increase temperature quickly to expand search area
+            f = copy(fopt)
+            x = copy(xopt)
+        else  # coverage not ok - increase temperature quickly to expand search area
             t = 10. * t
             for i = neps:-1:2
                 fstar[i] = fstar[i-1]
             end    
-			f = fopt
-			x = xopt
+            f = fopt
+            x = xopt
         end
     end
     return xopt, fopt, converge, details
