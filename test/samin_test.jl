925c377d9cdd06ec4df848363b364a3221fa3c1e
Michael Creel <michael.creel@uab.es>
2016-02-29 09:58:00 +0100

    add a bit of explanation

--- test/samin_test.jl
+++ test/samin_test.jl
@@ -11,14 +11,21 @@ lb = -ones(k,1)
 ub = -lb
 
 # converge to global opt
-@time xopt = samin(sse, x, lb, ub)
+println("normal convergence, terse output")
+@time xopt = samin(sse, x, lb, ub, verbosity=1)
+# converge to global opt, see final parameters
+println("normal convergence, more verbose output")
+@time xopt = samin(sse, x, lb, ub, verbosity=2)
 # no convergence within iteration limit
-@time xopt = samin(sse, x, lb, ub, maxevals=10)
+println("no convergence within iter limit")
+@time xopt = samin(sse, x, lb, ub, maxevals=10, verbosity=1)
 # initial point out of bounds
+println("initial point out of bounds")
 lb = 0.5*ub
 x[1,1] = 0.2
-xopt = samin(sse, x, lb, ub)
+xopt = samin(sse, x, lb, ub, verbosity=1)
 # optimum on bound of parameter space
+println("optimum on bounds of parameter space")
 x = 0.5 .+ 0.5*rand(k,1)
 xopt = samin(sse, x, lb, ub, verbosity=1)
 ;
