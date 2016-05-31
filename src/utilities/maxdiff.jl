0a7591b1ff285c1e38ad96c2943c6ab2a609980e
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2016-05-31 20:40:52 +0200

    Remove history.jl and add inbounds to maxdiff.jl.

--- src/utilities/maxdiff.jl
+++ src/utilities/maxdiff.jl
@@ -1,6 +1,6 @@
 function maxdiff(x::Array, y::Array)
     res = zero(x[1] - y[1])
-    for i in 1:length(x)
+    @inbounds for i in 1:length(x)
         delta = abs(x[i] - y[i])
         if delta > res
             res = delta
