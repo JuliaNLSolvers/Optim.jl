244d032eee487101268a2e683643c96122034421
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2016-05-31 10:03:35 +0200

    Adjustment in test/constrained.jl (#214)

--- test/constrained.jl
+++ test/constrained.jl
@@ -45,11 +45,12 @@ x0 = (rand(N)-0.5)*boxl
 for _optimizer in (ConjugateGradient, GradientDescent, LBFGS, BFGS)
     results = Optim.optimize(objective, x0, l, u, Fminbox(), optimizer = _optimizer)
     @test Optim.converged(results)
-end
-g = similar(x0)
-objective.fg!(Optim.minimizer(results), g)
-for i = 1:N
-    @test abs(g[i]) < 3e-3 || (Optim.minimizer(results)[i] < -boxl+1e-3 && g[i] > 0) || (Optim.minimizer(results)[i] > boxl-1e-3 && g[i] < 0)
+
+    g = similar(x0)
+    objective.fg!(Optim.minimizer(results), g)
+    for i = 1:N
+        @test abs(g[i]) < 3e-3 || (Optim.minimizer(results)[i] < -boxl+1e-3 && g[i] > 0) || (Optim.minimizer(results)[i] > boxl-1e-3 && g[i] < 0)
+    end
 end
 
 # tests for #180
