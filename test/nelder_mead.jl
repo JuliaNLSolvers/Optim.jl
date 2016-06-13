90339813df87be913ec41ebce0617b443968a855
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2016-06-13 13:10:37 +0200

    Implement adaptive parameters version of Nelder-Mead, and improve documentation.

--- test/nelder_mead.jl
+++ test/nelder_mead.jl
@@ -3,10 +3,13 @@ using Optim
 # Test Optim.nelder_mead for all functions except Large Polynomials in Optim.UnconstrainedProblems.examples
 for (name, prob) in Optim.UnconstrainedProblems.examples
 	f_prob = prob.f
-	res = Optim.optimize(f_prob, prob.initial_x, method=NelderMead())
-	if name != "Large Polynomial" # FIX THIS!
-		@assert norm(res.minimum - prob.solutions) < 1e-2
+	res = Optim.optimize(f_prob, prob.initial_x, NelderMead(), OptimizationOptions(iterations = 10000))
+	if name == "Powell"
+		res = Optim.optimize(f_prob, prob.initial_x, method=NelderMead(), g_tol = 1e-12)
+	elseif name == "Large Polynomial"
+		res = Optim.optimize(f_prob, prob.initial_x, method=NelderMead(initial_simplex = Optim.AffineSimplexer(1.,1.)), iterations = 500_000)
 	end
+	@assert norm(res.minimum - prob.solutions) < 1e-2
 end
 
 function f_nm(x::Vector)
