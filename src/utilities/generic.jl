f789d15c006fca0512c52f02bbd24641ff260f5a
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2016-12-12 20:07:53 +0100

    Fix macro hygene in generic.

--- src/utilities/generic.jl
+++ src/utilities/generic.jl
@@ -1,32 +1,34 @@
+macro def(name, definition)
+  esc(quote
+    macro $name()
+      esc($(Expr(:quote, definition)))
+    end
+  end)
+end
+
 # TODO decide if this is wanted and/or necessary
-macro add_generic_fields()
-    quote
-        method_string::String
-        n::Int64
-        x::Array{T}
-        f_x::T
-        f_calls::Int64
-        g_calls::Int64
-        h_calls::Int64
-    end
+@def add_generic_fields begin
+    method_string::String
+    n::Int64
+    x::Array{T}
+    f_x::T
+    f_calls::Int64
+    g_calls::Int64
+    h_calls::Int64
 end
 
-macro add_linesearch_fields()
-    quote
-        x_ls::Array{T}
-        g_ls::Array{T}
-        alpha::T
-        mayterminate::Bool
-        lsr::LineSearches.LineSearchResults
-    end
+@def add_linesearch_fields begin
+    x_ls::Array{T}
+    g_ls::Array{T}
+    alpha::T
+    mayterminate::Bool
+    lsr::LineSearches.LineSearchResults
 end
 
-macro initial_linesearch()
-    quote
-        (similar(initial_x), # Buffer of x for line search in state.x_ls
-        similar(initial_x), # Buffer of g for line search in state.g_ls
-        LineSearches.alphainit(one(T), initial_x, g, f_x), # Keep track of step size in state.alpha
-        false, # state.mayterminate
-        LineSearches.LineSearchResults(T))
-    end
+@def initial_linesearch begin
+    (similar(initial_x), # Buffer of x for line search in state.x_ls
+    similar(initial_x), # Buffer of g for line search in state.g_ls
+    LineSearches.alphainit(one(T), initial_x, g, f_x), # Keep track of step size in state.alpha
+    false, # state.mayterminate
+    LineSearches.LineSearchResults(T))
 end
