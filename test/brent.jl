4cf4cb2fd59bfe7265330bc75e1a3bb6fe222df5
Andrew Clausen <andrew.clausen@ed.ac.uk>
2017-01-08 13:15:24 +0000

    refactor univariate tests

--- test/brent.jl
+++ test/brent.jl
@@ -1,15 +1,10 @@
 let
     for (name, prob) in Optim.UnivariateProblems.examples
-        results = optimize(prob.f, prob.bounds..., method = Brent())
+        for T in (Float64, BigFloat)
+            results = optimize(prob.f, convert(Array{T}, prob.bounds)..., method = Brent())
 
-        @assert Optim.converged(results)
-        @assert norm(Optim.minimizer(results) - prob.minimizers) < 1e-7
-    end
-
-    for (name, prob) in Optim.UnivariateProblems.examples
-        results = optimize(prob.f, convert(Array{BigFloat}, prob.bounds)..., method = Brent())
-
-        @assert Optim.converged(results)
-        @assert norm(Optim.minimizer(results) - prob.minimizers) < 1e-7
+            @assert Optim.converged(results)
+            @assert norm(Optim.minimizer(results) - prob.minimizers) < 1e-7
+        end
     end
 end
