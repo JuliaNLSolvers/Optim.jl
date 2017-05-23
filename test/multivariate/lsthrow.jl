63f41d8df9fb2ed96986d023fe2f5fa81cd3277c
Asbjørn Nilsen Riseth <riseth@maths.ox.ac.uk>
2017-05-23 02:32:26 +0100

    Fix test linesearch function

--- test/multivariate/lsthrow.jl
+++ test/multivariate/lsthrow.jl
@@ -1,6 +1,6 @@
 @testset "Line search errors" begin
-    function ls(df,x,s,xtmp,g,lsr,c,mayterminate)
-        LineSearches.hagerzhang!(df,x,s,xtmp,g,lsr,c,mayterminate,
+    function ls(df,x,s,xtmp,lsr,c,mayterminate)
+        LineSearches.hagerzhang!(df,x,s,xtmp,lsr,c,mayterminate,
                                  0.1,0.9,
                                  Inf,5.,1e-6,0.66,
                                  2)
