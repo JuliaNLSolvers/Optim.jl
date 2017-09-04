df76dcf51a6fc800ec9dab5751465da9a7a55456
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2017-09-04 21:31:52 +0200

    docstring for GradientDescent.

--- src/multivariate/solvers/first_order/gradient_descent.jl
+++ src/multivariate/solvers/first_order/gradient_descent.jl
@@ -6,6 +6,25 @@ end
 
 Base.summary(::GradientDescent) = "Gradient Descent"
 
+"""
+# Gradient Descent
+## Constructor
+```julia
+GradientDescent(; linesearch = LineSearches.HagerZhang(),
+P = nothing,
+precondprep = (P, x) -> nothing)
+```
+Keywords are used to control choice of line search, and preconditioning.
+
+## Description
+The `GradientDescent` method a simple gradient descent algorithm, that is the
+search direction is simply the negative gradient at the current iterate, and
+then a line search step is used to compute the final step. See Nocedal and
+Wright (ch. 2.2, 1999) for an explanation of the approach.
+
+## References
+ - Nocedal, J. and Wright, S. J. (1999), Numerical optimization. Springer Science 35.67-68: 7.
+"""
 function GradientDescent(; linesearch = LineSearches.HagerZhang(),
                            P = nothing,
                            precondprep = (P, x) -> nothing)
