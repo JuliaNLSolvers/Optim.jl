9ad18203553d1a667c5a9759387590d5408c80df
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2017-10-01 11:39:25 +0200

    Add simple docstrings to Brent and GoldenSection.

--- src/univariate/solvers/brent.jl
+++ src/univariate/solvers/brent.jl
@@ -21,6 +21,24 @@ macro brenttrace()
     end)
 end
 
+"""
+# Brent
+## Constructor
+```julia
+    Brent(;)
+```
+
+## Description
+Also known as the Brent-Dekker algorith, `Brent` is a univariate optimization
+algorithm for minimizing functions on some interval `[a,b]`. The method uses bisection
+to find a zero of the gradient. If the original interval contains a minimum,
+bisection will reliably find the solution, but can be slow. To this end `Brent`
+combines bisection with the secant method and inverse quadratic interpolation to
+accelerate convergence.
+
+## References
+R. P. Brent (2002) Algorithms for Minimization Without Derivatives. Dover edition.
+"""
 struct Brent <: Optimizer end
 
 Base.summary(::Brent) = "Brent's Method"
