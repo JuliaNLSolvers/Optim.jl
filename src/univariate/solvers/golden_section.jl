554857122a7bfd0e45bc711107b3ca239681d1bb
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2017-10-01 17:55:33 +0200

    Add simple docstrings to Brent and GoldenSection. (#468)

--- src/univariate/solvers/golden_section.jl
+++ src/univariate/solvers/golden_section.jl
@@ -20,6 +20,22 @@ macro goldensectiontrace()
     end)
 end
 
+"""
+# GoldenSection
+## Constructor
+```julia
+    GoldenSection(;)
+```
+
+## Description
+The `GoldenSection` method seeks to minimize a univariate function on an interval
+`[a, b]`. At all times the algorithm maintains a tuple of three minimizer candidates
+`(c, d, e)` where ``c<d<e`` such that the ratio of the largest to the smallest interval
+is the Golden Ratio.
+
+## References
+https://en.wikipedia.org/wiki/Golden-section_search
+"""
 struct GoldenSection <: Optimizer end
 
 Base.summary(::GoldenSection) = "Golden Section Search"
