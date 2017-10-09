cd6a2866ed6cd27886ea4215ffd8acfc78d7ad9f
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2017-10-09 20:55:55 +0200

    debug at av

--- test/multivariate/solvers/first_order/momentum_gradient_descent.jl
+++ test/multivariate/solvers/first_order/momentum_gradient_descent.jl
@@ -4,5 +4,5 @@
                     convergence_exceptions = (("Large Polynomial",1),
                      ("Himmelblau",1), ("Powell", 1)),
                     minimizer_exceptions = (("Powell", 2),),
-                    f_increase_exceptions = ("Exponential",))
+                    f_increase_exceptions = ("Exponential", "Polynomial"))
 end
