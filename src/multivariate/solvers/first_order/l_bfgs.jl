e201b5964dcdd184353adc178b436fe645dcad01
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2017-11-03 09:05:29 +0000

    Fix dots

--- src/multivariate/solvers/first_order/l_bfgs.jl
+++ src/multivariate/solvers/first_order/l_bfgs.jl
@@ -49,7 +49,7 @@ function twoloop!(s::Vector,
         sidx = view(dx_history, :, idx) # TODO: should these use devec_fun?
         yidx = view(dg_history, :, idx) # TODO: should these use devec_fun?
         scaling = dot(sidx, yidx) / sum(abs2, yidx) # TODO: Should this be vecdot?
-        s .= scaling*q
+        @. s = scaling*q
     else
         A_ldiv_B!(devec_fun(s), precon, devec_fun(q))
     end
@@ -88,22 +88,28 @@ alphaguess = LineSearches.InitialStatic(),
 linesearch = LineSearches.HagerZhang(),
 P=nothing,
 precondprep = (P, x) -> nothing,
-manifold = Flat())
+manifold = Flat(),
+scaleinvH::Bool = true)
 ```
-`LBFGS` has a special keyword; the memory length `m`.
+`LBFGS` has two special keywords; the memory length `m`,
+and the `scaleinvH0` flag.
 The memory length determines how many previous Hessian
 approximations to store.
-In addition, it supports preconditioning via the `P` and `precondprep`
+When `scaleinvH0 == true`,
+then the initial guess in the two-loop recursion to approximate the
+inverse Hessian is the scaled identity, as can be found in Nocedal and Wright (2nd edition) (sec. 7.2).
+
+In addition, LBFGS supports preconditioning via the `P` and `precondprep`
 keywords.
 
 ## Description
 The `LBFGS` method implements the limited-memory BFGS algorithm as described in
-Nocedal and Wright (sec. 9.1, 1999) and original paper by Liu & Nocedal (1989).
+Nocedal and Wright (sec. 7.2, 2006) and original paper by Liu & Nocedal (1989).
 It is a quasi-Newton method that updates an approximation to the Hessian using
 past approximations as well as the gradient.
 
 ## References
- - Wright, S. J. and J. Nocedal (1999), Numerical optimization. Springer Science 35.67-68: 7.
+ - Wright, S. J. and J. Nocedal (2006), Numerical optimization, 2nd edition. Springer
  - Liu, D. C. and Nocedal, J. (1989). "On the Limited Memory Method for Large Scale Optimization". Mathematical Programming B. 45 (3): 503–528
 """
 function LBFGS(; m::Integer = 10,
