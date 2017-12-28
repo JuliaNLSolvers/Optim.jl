5099dd4b01ea1ee590a6e7f855f8173535730b75
Kenta Sato (佐藤 建太) <bicycle1885@gmail.com>
2017-12-29 00:22:56 +0900

    flush trace log (#502)

--- src/utilities/update.jl
+++ src/utilities/update.jl
@@ -14,6 +14,7 @@ function update!(tr::OptimizationTrace{T},
     if show_trace
         if iteration % show_every == 0
             show(os)
+            flush(STDOUT)
         end
     end
     if callback != nothing && (iteration % show_every == 0)
