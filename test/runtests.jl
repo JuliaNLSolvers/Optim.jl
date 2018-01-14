c507f669a5961a29386c018585d40eb2fff08bce
Asbjørn Nilsen Riseth <riseth@maths.ox.ac.uk>
2018-01-14 12:08:04 +0000

    Use finite difference if problem is not differentiable

--- test/runtests.jl
+++ test/runtests.jl
@@ -66,23 +66,25 @@ multivariate_tests = [
 ]
 multivariate_tests = map(s->"./multivariate/"*s*".jl", multivariate_tests)
 
-differentiability_condition(method, prob) = true
-differentiability_condition(method::Optim.FirstOrderOptimizer, prob) = prob.isdifferentiable
-differentiability_condition(method::Optim.SecondOrderOptimizer, prob) = prob.istwicedifferentiable
-
-input_tuple(method, prob) = ((prob.f,),)
-input_tuple(method::Optim.FirstOrderOptimizer, prob) = ((prob.f,), (prob.f, prob.g!))
-input_tuple(method::Optim.SecondOrderOptimizer, prob) = ((prob.f,), (prob.f, prob.g!), (prob.f, prob.g!, prob.h!))
+input_tuple(method, prob) = ((UP.objective(prob),),)
+input_tuple(method::Optim.FirstOrderOptimizer, prob) = ((UP.objective(prob),), (UP.objective(prob), UP.gradient(prob)))
+input_tuple(method::Optim.SecondOrderOptimizer, prob) = ((UP.objective(prob),), (UP.objective(prob), UP.gradient(prob)), (UP.objective(prob), UP.gradient(prob), UP.hessian(prob)))
 
 function run_optim_tests(method; convergence_exceptions = (),
-                                 minimizer_exceptions = (),
-                                 f_increase_exceptions = (),
-                                 iteration_exceptions = (),
-                                 skip = (),
-                                 show_name = false,
-                                 show_trace = false)
+                         minimizer_exceptions = (),
+                         minimum_exceptions = (),
+                         f_increase_exceptions = (),
+                         iteration_exceptions = (),
+                         skip = (),
+                         show_name = false,
+                         show_trace = false,
+                         show_res = false)
     # Loop over unconstrained problems
     for (name, prob) in OptimTestProblems.UnconstrainedProblems.examples
+        if !isfinite(prob.minimum) || !any(isfinite, prob.solutions)
+            debug_printing && println("$name has no registered minimum/minimizer. Skipping ...")
+            continue
+        end
         show_name && print_with_color(:green, "Problem: ", name, "\n")
         # Look for name in the first elements of the iteration_exceptions tuples
         iter_id = find(n[1] == name for n in iteration_exceptions)
@@ -90,19 +92,30 @@ function run_optim_tests(method; convergence_exceptions = (),
         iters = length(iter_id) == 0 ? 1000 : iteration_exceptions[iter_id[1]][2]
         # Construct options
         options = Optim.Options(allow_f_increases = name in f_increase_exceptions, iterations = iters, show_trace = show_trace)
-        # Check if once or twice differentiable
-        if differentiability_condition(method, prob) && !(name in skip)
-            # Loop over appropriate input combinations of f, g!, and h!
+
+        # Use finite difference if it is not differentiable enough
+        if  !(name in skip)
             for (i, input) in enumerate(input_tuple(method, prob))
+                if (!prob.isdifferentiable && i > 1) || (!prob.istwicedifferentiable && i > 2)
+                    continue
+                end
+
+                # Loop over appropriate input combinations of f, g!, and h!
                 results = Optim.optimize(input..., prob.initial_x, method, options)
                 @test isa(summary(results), String)
+                show_res && display(results)
                 if !((name, i) in convergence_exceptions)
                     @test Optim.converged(results)
+                end
+                if !((name, i) in minimum_exceptions)
+                    @test Optim.minimum(results) < prob.minimum + sqrt(eps(typeof(prob.minimum)))
                 end
                 if !((name, i) in minimizer_exceptions)
                     @test norm(Optim.minimizer(results) - prob.solutions) < 1e-2
                 end
             end
+        else
+            debug_printing && print_with_color(:blue, "Skipping $name\n")
         end
     end
 end
