22a71aa7b0cdc0302d4f628cfa84a37a9b7937dd
Asbjørn Nilsen Riseth <riseth@maths.ox.ac.uk>
2018-01-23 22:16:55 +0000

    Make FD timing tests smaller

--- test/multivariate/fdtime.jl
+++ test/multivariate/fdtime.jl
@@ -2,68 +2,40 @@
     fd_input_tuple(method::Optim.FirstOrderOptimizer, prob) = ((UP.objective(prob),),)
     fd_input_tuple(method::Optim.SecondOrderOptimizer, prob) = ((UP.objective(prob),), (UP.objective(prob), UP.gradient(prob)))
 
-    function run_optim_fd_tests(method; convergence_exceptions = (),
-                                minimizer_exceptions = (),
-                                minimum_exceptions = (),
-                                f_increase_exceptions = (),
-                                iteration_exceptions = (),
-                                skip = (),
-                                show_name = false,
-                                show_trace = false,
-                                show_res = false)
+    function run_optim_fd_tests(method;
+                                problems = ("Extended Rosenbrock", "Large Polynomial", "Powell",
+                                            "Paraboloid Diagonal"),
+                                show_name = false, show_trace = false,
+                                show_time = false, show_res = false)
+
         # Loop over unconstrained problems
-        for (name, prob) in OptimTestProblems.UnconstrainedProblems.examples
-            if !isfinite(prob.minimum) || !any(isfinite, prob.solutions)
-                debug_printing && println("$name has no registered minimum/minimizer. Skipping ...")
-                continue
-            end
+        for name in problems
+            prob = UP.examples[name]
             show_name && print_with_color(:green, "Problem: ", name, "\n")
-            # Look for name in the first elements of the iteration_exceptions tuples
-            iter_id = find(n[1] == name for n in iteration_exceptions)
-            # If name wasn't found, use default 1000 iterations, else use provided number
-            iters = length(iter_id) == 0 ? 1000 : iteration_exceptions[iter_id[1]][2]
-            # Construct options
-            options = Optim.Options(allow_f_increases = name in f_increase_exceptions, iterations = iters, show_trace = show_trace)
+            options = Optim.Options(show_trace = show_trace)
+            for (i, input) in enumerate(fd_input_tuple(method, prob))
+                # Loop over appropriate input combinations of f, g!, and h!
+                results = Optim.optimize(input..., prob.initial_x, method, options)
 
-            # Use finite difference if it is not differentiable enough
-            if  !(name in skip)
-                for (i, input) in enumerate(fd_input_tuple(method, prob))
-                    if (!prob.isdifferentiable && i > 1) || (!prob.istwicedifferentiable && i > 2)
-                        continue
-                    end
+                debug_printing && print_with_color(:red, "f-calls: $(Optim.f_calls(results))\n")
+                show_res && display(results)
 
-                    # Loop over appropriate input combinations of f, g!, and h!
-                    results = Optim.optimize(input..., prob.initial_x, method, options)
-                    debug_printing && print_with_color(:red, "f-calls: $(Optim.f_calls(results))\n")
-                    @time Optim.optimize(input..., prob.initial_x, method, options)
+                show_time && @time Optim.optimize(input..., prob.initial_x, method, options)
 
-                    show_res && println(results)
-                    if !((name, i) in convergence_exceptions)
-                        @test Optim.converged(results)
-                    end
-                    if !((name, i) in minimum_exceptions)
-                        @test Optim.minimum(results) < prob.minimum + sqrt(eps(typeof(prob.minimum)))
-                    end
-                    if !((name, i) in minimizer_exceptions)
-                        @test norm(Optim.minimizer(results) - prob.solutions) < 1e-2
-                    end
-                end
-            else
-                debug_printing && print_with_color(:blue, "Skipping $name\n")
+                @test Optim.converged(results)
+                @test Optim.minimum(results) < prob.minimum + sqrt(eps(typeof(prob.minimum)))
+                @test norm(Optim.minimizer(results) - prob.solutions) < 1e-2
             end
         end
     end
 
-    skip = ("Trigonometric",)
     @testset "Timing with LBFGS" begin
         debug_printing && print_with_color(:blue, "#####################\nSolver: L-BFGS\n")
-        run_optim_fd_tests(LBFGS(), skip = skip,
-                           show_name=debug_printing)
+        run_optim_fd_tests(LBFGS(), show_name=debug_printing, show_time = debug_printing)
     end
 
     @testset "Timing with Newton" begin
         debug_printing && print_with_color(:blue, "#####################\nSolver: Newton\n")
-        run_optim_fd_tests(Newton(), skip = skip,
-                           show_name=debug_printing)
+        run_optim_fd_tests(Newton(), show_name=debug_printing, show_time = debug_printing)
     end
 end
