d77f269261ab265ca994684ce2ae6bf924293ba4
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2018-03-22 14:02:24 +0100

    Add simulated annealing with bounds. (#553)
    * Add simulated annealing with bounds.
    
    * Changes.
    
    * Error on out of bounds.
    
    * Fix tests.

---
+++ test/multivariate/solvers/constrained/samin.jl
@@ -0,0 +1,8 @@
+@testset "SAMIN" begin
+    prob = MVP.UnconstrainedProblems.examples["Himmelblau"]
+
+    xtrue = prob.solutions
+    f = OptimTestProblems.MultivariateProblems.objective(prob)
+    x0 = prob.initial_x
+    optimize(f, x0, x0.-100., x0.+100.0, Optim.SAMIN())
+end
