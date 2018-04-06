05c76ddcc93a96dcbbe89aa9f56aa4d195e00d62
Michael Creel <michael.creel@uab.cat>
2018-04-06 11:37:22 +0200

    samin changes: defaults, verbosity, and documentation (#558)
    * Update samin.md
    
    The example was "quenching" because the cooling rate was too fast. This small change obtains a much more accurate solution.
    
    * more detailed samin documentation
    
    * change some defaults, modify verbosity
    
    * the default rt has been set to 0.9, which is more conservative, and more likely to obtain a correct solution
    * the final results are displayed, until the convergence of samin can be integrated into the general scheme. Otherwise, it appears that the algorithm fails to converge, when in fact it does.
    
    * typos
    
    * fix test for SAMIN

--- test/multivariate/solvers/constrained/samin.jl
+++ test/multivariate/solvers/constrained/samin.jl
@@ -4,6 +4,6 @@
     xtrue = prob.solutions
     f = OptimTestProblems.MultivariateProblems.objective(prob)
     x0 = prob.initial_x
-    res = optimize(f, x0, x0.-100., x0.+100.0, Optim.SAMIN(), Optim.Options(iterations=4000))
+    optimize(f, x0, x0.-100., x0.+100.0, Optim.SAMIN(), Optim.Options(iterations=1000000))
     @test Optim.minimum(res) < 1e-6
 end
