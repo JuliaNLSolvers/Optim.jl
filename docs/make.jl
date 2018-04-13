8d1c56411a49c07be6863cfe53f9ed7c75b7f5d5
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2018-04-13 16:33:38 +0200

    Fix license md in docs.

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,5 @@
 using Documenter, Optim
 
-# use include("Rosenbrock.jl") etc
-
-# assuming linux.
-run(`mv ../LICENSE.md ./LICENSE.md`)
-#run('mv ../CONTRIBUTING.md ./dev/CONTRIBUTING.md')
 makedocs(
     doctest = false
 )
