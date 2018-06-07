dfa3efdaf738a34cd4eb4261fca1db275df7d2f7
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2018-06-07 12:37:54 +0200

    some fixes

--- src/multivariate/solvers/constrained/fminbox.jl
+++ src/multivariate/solvers/constrained/fminbox.jl
@@ -99,7 +99,7 @@ function Fminbox(method::AbstractOptimizer = LBFGS();
                  mu0::Real = NaN, mufactor::Real = 0.001,
                  precondprep = (P, x, l, u, mu) -> precondprepbox!(P, x, l, u, mu))
     if method isa Newton || method isa NewtonTrustRegion
-        error("Newton is not supported as the Fminbox optimizer.")
+        throw(ArgumentError("Newton is not supported as the Fminbox optimizer."))
     end
     Fminbox(method, promote(mu0, mufactor)..., precondprep) # default optimizer
 end
@@ -139,7 +139,7 @@ function optimize(obj,
 end
 
 function optimize(f,
-                  g!,
+                  g,
                   l::AbstractArray{T},
                   u::AbstractArray{T},
                   initial_x::AbstractArray{T},
