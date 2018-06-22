d6db7c0ee1fa8842fb8dcf4a9b2d14df97ca1a6d
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2018-06-22 15:55:38 +0200

    Add Literate generator script (#634)
    * Add Literate generator script
    
    * Build and deploy the docs during tests

---
+++ docs/generate.jl
@@ -0,0 +1,19 @@
+# generate examples
+import Literate
+
+# TODO: Remove items from `SKIPFILE` as soon as they run on the latest
+# stable `Optim` (or other dependency)
+#ONLYSTATIC = ["ipnewton_basics.jl",]
+ONLYSTATIC = []
+
+EXAMPLEDIR = joinpath(@__DIR__, "src", "examples")
+GENERATEDDIR = joinpath(@__DIR__, "src", "examples", "generated")
+for example in filter!(r"\.jl$", readdir(EXAMPLEDIR))
+    input = abspath(joinpath(EXAMPLEDIR, example))
+    script = Literate.script(input, GENERATEDDIR)
+    code = strip(read(script, String))
+    mdpost(str) = replace(str, "@__CODE__" => code)
+    Literate.markdown(input, GENERATEDDIR, postprocess = mdpost,
+                      documenter = !(example in ONLYSTATIC))
+    Literate.notebook(input, GENERATEDDIR, execute = !(example in ONLYSTATIC))
+end
