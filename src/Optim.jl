8f328ec0493fd45ee93ae66e4839517796c66327
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2018-06-25 11:43:11 +0200

    Add a simple docstring to Optim module to tell users where to find help. (#638)

--- src/Optim.jl
+++ src/Optim.jl
@@ -1,5 +1,20 @@
 __precompile__(true)
+"""
+# Optim.jl
+Welcome to Optim.jl!
 
+Optim.jl is a package used to solve continuous optimization problems. It is
+written in Julia for Julians to help take advantage of arbitrary number types,
+fast computation, and excellent automatic differentiation tools.
+
+## REPL help
+`?` followed by an algorithm name (`?BFGS`), constructors (`?Optim.Options`)
+prints help to the terminal.
+
+## Documentation
+Besides the help provided at the REPL, it is possible to find help and general
+documentation online at http://julianlsolvers.github.io/Optim.jl/stable/ .
+"""
 module Optim
 using NLSolversBase          # for shared infrastructure in JuliaNLSolvers
 using PositiveFactorizations # for globalization strategy in Newton
