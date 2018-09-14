bc5b0e4a7a6003d75de8a7389bf23b22bee43fde
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2018-09-14 09:46:03 +0200

    Actually fix issue, and print results on convergence error

--- test/multivariate/solvers/first_order/l_bfgs.jl
+++ test/multivariate/solvers/first_order/l_bfgs.jl
@@ -4,19 +4,19 @@
 
     if Sys.WORD_SIZE == 32
         # Not sure why it doesn't converge; I don't have a system to check it out on
-        convergence_exceptions = (("Extended Powell", 1),
-                                  ("Extended Powell", 2))
+        #convergence_exceptions = (("Extended Powell", 1),
+        #("Extended Powell", 2))
+        iteration_exceptions = (("Extended Powell", 2000),)
     else
-        convergence_exceptions = ()
+        #convergence_exceptions = ()
+        iteration_exceptions = ()
     end
 
-    @show skip
-    @show convergence_exceptions
     run_optim_tests(LBFGS(),
                     f_increase_exceptions = ("Extended Rosenbrock",),
                     skip=skip,
-                    convergence_exceptions = convergence_exceptions,
+                    #convergence_exceptions = convergence_exceptions,
+                    iteration_exceptions = iteration_exceptions,
                     show_name = debug_printing,
-                    show_res = debug_printing,
                     )
 end
