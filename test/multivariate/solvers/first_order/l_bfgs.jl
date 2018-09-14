2e81c088232ce623f7f67af9ab1fff414aa4042e
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2018-09-14 10:07:58 +0200

    Re-enable all tests

--- test/multivariate/solvers/first_order/l_bfgs.jl
+++ test/multivariate/solvers/first_order/l_bfgs.jl
@@ -3,19 +3,14 @@
     skip = ("Trigonometric",)
 
     if Sys.WORD_SIZE == 32
-        # Not sure why it doesn't converge; I don't have a system to check it out on
-        #convergence_exceptions = (("Extended Powell", 1),
-        #("Extended Powell", 2))
         iteration_exceptions = (("Extended Powell", 2000),)
     else
-        #convergence_exceptions = ()
         iteration_exceptions = ()
     end
 
     run_optim_tests(LBFGS(),
                     f_increase_exceptions = ("Extended Rosenbrock",),
                     skip=skip,
-                    #convergence_exceptions = convergence_exceptions,
                     iteration_exceptions = iteration_exceptions,
                     show_name = debug_printing,
                     )
