bb258a629b4be48f9a5e40c75289dfbc42324179
Tyler Beason <tbeas12@gmail.com>
2019-08-14 09:01:56 -0700

    -fix tests

--- test/multivariate/solvers/zeroth_order/particle_swarm.jl
+++ test/multivariate/solvers/zeroth_order/particle_swarm.jl
@@ -15,7 +15,7 @@
     lower = [-100.0]
     n_particles = 4
     options = Optim.Options(iterations=100)
-    res = Optim.optimize(f_s, initial_x, ParticleSwarm(lower, upper, n_particles),
+    res = Optim.optimize(f_s, initial_x, ParticleSwarm(lower, upper, n_particles,false),
                          options)
     @test norm(Optim.minimizer(res) - [5.0]) < 0.1
 
@@ -24,12 +24,12 @@
     upper = [20., 20.]
     n_particles = 5
     options = Optim.Options(iterations=300)
-    res = Optim.optimize(rosenbrock_s, initial_x, ParticleSwarm(lower, upper, n_particles),
+    res = Optim.optimize(rosenbrock_s, initial_x, ParticleSwarm(lower, upper, n_particles,false),
                              options)
     @test norm(Optim.minimizer(res) - [1.0, 1.0]) < 0.1
     @suppress_out begin
         options = Optim.Options(iterations=300, show_trace=true, extended_trace=true, store_trace=true)
-        res = Optim.optimize(rosenbrock_s, initial_x, ParticleSwarm(lower, upper, n_particles), options)
+        res = Optim.optimize(rosenbrock_s, initial_x, ParticleSwarm(lower, upper, n_particles,false), options)
         @test summary(res) == "Particle Swarm"
         res = Optim.optimize(rosenbrock_s, initial_x, ParticleSwarm(n_particles = n_particles), options)
         @test summary(res) == "Particle Swarm"
