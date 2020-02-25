378573e8d1daf5416e8d2767ced33f5795f0912f
Josua Grawitter <josh@greyage.org>
2020-02-25 18:11:49 +0100

    Actually stop at line search failure

--- src/multivariate/optimize/optimize.jl
+++ src/multivariate/optimize/optimize.jl
@@ -54,7 +54,7 @@ function optimize(d::D, initial_x::Tx, method::M,
     while !converged && !stopped && iteration < options.iterations
         iteration += 1
 
-        ls_failed = update_state!(d, state, method)
+        ls_success = !update_state!(d, state, method)
         if !ls_success
             break # it returns true if it's forced by something in update! to stop (eg dx_dg == 0.0 in BFGS, or linesearch errors)
         end
