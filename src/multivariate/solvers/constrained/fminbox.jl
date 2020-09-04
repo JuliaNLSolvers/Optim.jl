e9fffe43d2320b253c377a9a3a138b39cece7804
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2020-09-04 14:32:41 +0200

    Update docstring

--- src/multivariate/solvers/constrained/fminbox.jl
+++ src/multivariate/solvers/constrained/fminbox.jl
@@ -156,6 +156,13 @@ Fminbox(method;
         mufactor=0.0001,
         precondprep(P, x, l, u, mu) -> precondprepbox!(P, x, l, u, mu))
 ```
+## Description
+Fminbox implements a primal barrier method for optimization with simple
+bounds (or box constraints). A description of an approach very close to
+the one implemented here can be found in section 19.6 of Nocedal and Wright
+ (sec. 19.6, 2006).
+## References
+ - Wright, S. J. and J. Nocedal (1999), Numerical optimization. Springer Science 35.67-68: 7.
 """
 function Fminbox(method::AbstractOptimizer = LBFGS();
                  mu0::Real = NaN, mufactor::Real = 0.001,
