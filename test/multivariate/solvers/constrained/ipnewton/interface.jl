ab60867add6f2bdb7b7f2a3b3d31f6d66e3b57b8
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2020-09-06 21:35:52 +0200

    Make it easier to use IPNewton if you only have bounds. (#858)
    * Allow for optimize(obj, lower, upper, initial_x, ::IPNewton) call
    
    * Allow to optimize AbstractObjective, functions with only bounds (not an explicit constraint).
    
    * Easy IPNewton
    
    * Use ForwardDiff from NLSolversBase.
    
    * Add a description in the docs.

--- test/multivariate/solvers/constrained/ipnewton/interface.jl
+++ test/multivariate/solvers/constrained/ipnewton/interface.jl
@@ -1,3 +1,4 @@
+using Optim, Test
 @testset "#711" begin
 	# make sure it doesn't try to promote df
 	dof = 7
@@ -10,4 +11,42 @@
 
 	res = optimize(df, dfc, x0, IPNewton(); autodiff=:forward)
 	res = optimize(df, dfc, x0, IPNewton())
+end
+
+@testset "#600" begin
+	function exponential(x)
+		return exp((2.0 - x[1])^2) + exp((3.0 - x[2])^2)
+	end
+
+	function exponential_gradient!(storage, x)
+		storage[1] = -2.0 * (2.0 - x[1]) * exp((2.0 - x[1])^2)
+		storage[2] = -2.0 * (3.0 - x[2]) * exp((3.0 - x[2])^2)
+		storage
+	end
+	function exponential_hessian!(storage, x)
+		Optim.NLSolversBase.ForwardDiff.hessian!(storage, exponential, x)
+	end
+
+	function exponential_gradient(x)
+		storage = similar(x)
+		storage[1] = -2.0 * (2.0 - x[1]) * exp((2.0 - x[1])^2)
+		storage[2] = -2.0 * (3.0 - x[2]) * exp((3.0 - x[2])^2)
+		storage
+	end
+
+	initial_x = [0.0, 0.0]
+	optimize(exponential, exponential_gradient!, initial_x, BFGS())
+	lb = fill(-0.1, 2)
+	ub = fill(1.1, 2)
+	od = OnceDifferentiable(exponential, initial_x)
+	optimize(od, lb, ub, initial_x, IPNewton())
+	optimize(od, lb, ub, initial_x, IPNewton(), Optim.Options())
+	optimize(exponential, lb, ub, initial_x, IPNewton())
+	optimize(exponential, lb, ub, initial_x, IPNewton(), Optim.Options())
+	optimize(exponential, exponential_gradient!, lb, ub, initial_x, IPNewton())
+	optimize(exponential, exponential_gradient!, lb, ub, initial_x, IPNewton(), Optim.Options())
+	optimize(exponential, exponential_gradient!, exponential_hessian!, lb, ub, initial_x, IPNewton())
+	optimize(exponential, exponential_gradient!, exponential_hessian!, lb, ub, initial_x, IPNewton(), Optim.Options())
+	optimize(TwiceDifferentiable(od, initial_x), lb, ub, initial_x)
+	optimize(TwiceDifferentiable(od, initial_x), lb, ub, initial_x, Optim.Options())
 end
