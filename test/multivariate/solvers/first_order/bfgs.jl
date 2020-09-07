0eb4f686c26da5e7f0a8e594d84cc7dc41bc45fe
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2020-09-07 10:32:18 +0200

    Test #860

--- test/multivariate/solvers/first_order/bfgs.jl
+++ test/multivariate/solvers/first_order/bfgs.jl
@@ -1,3 +1,4 @@
+using Optim, Test
 @testset "BFGS" begin
     # Trigonometric gets stuck in a local minimum?
     skip = ("Trigonometric",)
@@ -5,4 +6,20 @@
                     f_increase_exceptions = ("Extended Rosenbrock",),
                     skip=skip,
                     show_name = debug_printing)
+end
+@testset "reset" begin
+    rosenbrock = MultivariateProblems.UnconstrainedProblems.examples["Rosenbrock"]
+    f = MVP.objective(rosenbrock)
+    g! = MVP.gradient(rosenbrock)
+    h! = MVP.hessian(rosenbrock)
+
+    initial_x = rosenbrock.initial_x
+    
+    d = OnceDifferentiable(f, g!, initial_x)
+
+    options= Optim.Options()
+    state = Optim.initial_state(method, options, d, initial_x)
+    initial_invH = [ 0.0004638218923933211, 0.0004638218923933211]
+    @test all(state.invH .== initial_invH)
+    op_res  = optimize(d, initial_x, method, options, state)
 end
