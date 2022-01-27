b6e9f4b6a10e43edda199a2ccad0ddb69fd49643
Vedant Puri <vedantpuri@gmail.com>
2022-01-27 16:59:15 +0530

    `mul!` is faster than `@simd`
    MWE: https://github.com/JuliaNLSolvers/Optim.jl/commit/331a26fd76011b1e4598fc622a86c4cc6797307f#commitcomment-64720042

--- src/multivariate/solvers/first_order/bfgs.jl
+++ src/multivariate/solvers/first_order/bfgs.jl
@@ -160,19 +160,9 @@ function update_h!(d, state, method::BFGS)
         c2 = 1 / dx_dg
 
         # invH = invH + c1 * (s * s') - c2 * (u * s' + s * u')
-        if(state.invH isa Array) # i.e. not a CuArray
-            for i in 1:n
-                @simd for j in 1:n
-                    @inbounds state.invH[i, j] += c1 * state.dx[i] * state.dx[j]' -
-                                                  c2 * (state.u[i] * state.dx[j]' +
-                                                        state.u[j]' * state.dx[i])
-                end
-            end
-        else
-            mul!(state.invH,vec(state.dx),vec(state.dx)', c1,1)
-            mul!(state.invH,vec(state.u ),vec(state.dx)',-c2,1)
-            mul!(state.invH,vec(state.dx),vec(state.u )',-c2,1)
-        end
+        mul!(state.invH,vec(state.dx),vec(state.dx)', c1,1)
+        mul!(state.invH,vec(state.u ),vec(state.dx)',-c2,1)
+        mul!(state.invH,vec(state.dx),vec(state.u )',-c2,1)
     end
 end
 
