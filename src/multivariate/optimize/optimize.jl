8f5c5f94964e3d32e2ee767df05e61e6643d9099
Mohamed Tarek <mohamed82008@gmail.com>
2023-08-10 05:32:55 +1000

    exit early when nan in grad or hessian

--- src/multivariate/optimize/optimize.jl
+++ src/multivariate/optimize/optimize.jl
@@ -57,6 +57,10 @@ function optimize(d::D, initial_x::Tx, method::M,
         end
         if !(method isa NewtonTrustRegion)
             update_g!(d, state, method) # TODO: Should this be `update_fg!`?
+            if !all(isfinite, gradient(d))
+                @warn "Terminated early due to NaN in gradient."
+                break
+            end
         end
         x_converged, f_converged,
         g_converged, f_increased = assess_convergence(state, d, options)
@@ -66,6 +70,10 @@ function optimize(d::D, initial_x::Tx, method::M,
         converged = x_converged || g_converged || (counter_f_tol > options.successive_f_tol)
         if !(converged && method isa Newton) && !(method isa NewtonTrustRegion)
             update_h!(d, state, method) # only relevant if not converged
+            if !all(isfinite, hessian(d))
+                @warn "Terminated early due to NaN in Hessian."
+                break
+            end
         end
         if tracing
             # update trace; callbacks can stop routine early by returning true
