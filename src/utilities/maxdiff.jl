7fbfb36d8fc42c79e352231ffa414d9d7552855c
annuges <33180039+annuges@users.noreply.github.com>
2023-10-07 19:07:09 +0200

    restore allocation free maxdiff (#1051)
    The mapreduce based version allocates which is undesirable.
    This change restores the previous allocation free code for normal Arrays while still allowing gpu arrays using mapreduce

--- src/utilities/maxdiff.jl
+++ src/utilities/maxdiff.jl
@@ -1,3 +1,16 @@
+# generic version for gpu support
 function maxdiff(x::AbstractArray, y::AbstractArray)
     return mapreduce((a, b) -> abs(a - b), max, x, y)
+end
+
+# allocation free version for normal arrays
+function maxdiff(x::Array, y::Array)
+    res = real(zero(x[1] - y[1]))
+    @inbounds for i in 1:length(x)
+        delta = abs(x[i] - y[i])
+        if delta > res
+            res = delta
+        end
+    end
+    return res
 end
