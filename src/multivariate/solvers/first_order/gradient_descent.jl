6122d29bb0e9fbf56b9afd84f30bf19106401e89
Lukas Arnroth <lukas.arnroth@gmail.com>
2024-01-16 22:09:42 +0100

    Grammar in documentation of GradientDescent() (#1061)

--- src/multivariate/solvers/first_order/gradient_descent.jl
+++ src/multivariate/solvers/first_order/gradient_descent.jl
@@ -20,7 +20,7 @@ precondprep = (P, x) -> nothing)
 Keywords are used to control choice of line search, and preconditioning.
 
 ## Description
-The `GradientDescent` method a simple gradient descent algorithm, that is the
+The `GradientDescent` method is a simple gradient descent algorithm, that is the
 search direction is simply the negative gradient at the current iterate, and
 then a line search step is used to compute the final step. See Nocedal and
 Wright (ch. 2.2, 1999) for an explanation of the approach.
