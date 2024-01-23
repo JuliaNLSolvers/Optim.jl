ccec09c3906ed2eeed281673513797005e6742e7
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2024-01-23 12:43:01 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -21,5 +21,4 @@ makedocs(
 deploydocs(
   #  deps = Deps.pip("pygments", "mkdocs", "python-markdown-math", "mkdocs-windmill"),
     repo = "github.com/JuliaNLSolvers/Optim.jl.git",
-    julia = "1"
 )
