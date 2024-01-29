de6b82017d078f019c6ddfaf0108700ec954f98d
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2024-01-29 12:47:51 +0100

    Update adamax.jl

--- src/multivariate/solvers/first_order/adamax.jl
+++ src/multivariate/solvers/first_order/adamax.jl
@@ -47,7 +47,7 @@ function initial_state(method::AdaMax, options, d, initial_x::AbstractArray{T}) 
    
     z = copy(initial_x)
     m = copy(gradient(d))
-    u = fill(zero(m[1]^2), length(m))
+    u = zeros(m)
     a = 1 - β₁
     iter = 0
 
