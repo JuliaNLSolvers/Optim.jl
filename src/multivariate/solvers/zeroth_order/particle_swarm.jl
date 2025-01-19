24a96fec60ca2be95eee49cb9d25f5223379cdcf
Simen Gaure <simen@gaure.no>
2025-01-19 11:22:03 +0100

    Add parallel support for ParticleSwarm

--- src/multivariate/solvers/zeroth_order/particle_swarm.jl
+++ src/multivariate/solvers/zeroth_order/particle_swarm.jl
@@ -2,7 +2,10 @@ struct ParticleSwarm{Tl, Tu} <: ZerothOrderOptimizer
     lower::Tl
     upper::Tu
     n_particles::Int
+    parallel::Bool
 end
+
+ParticleSwarm(a, b, c) = ParticleSwarm(a, b, c, false)
 
 """
 # Particle Swarm
@@ -10,13 +13,16 @@ end
 ```julia
 ParticleSwarm(; lower = [],
                 upper = [],
-                n_particles = 0)
+                n_particles = 0,
+                parallel = false)
 ```
 
-The constructor takes 3 keywords:
+The constructor takes 4 keywords:
 * `lower = []`, a vector of lower bounds, unbounded below if empty or `-Inf`'s
 * `upper = []`, a vector of upper bounds, unbounded above if empty or `Inf`'s
 * `n_particles = 0`, the number of particles in the swarm, defaults to least three
+* `parallel = false`, if true, the objective function is evaluated on a matrix
+  of column vectors. 
 
 ## Description
 The Particle Swarm implementation in Optim.jl is the so-called Adaptive Particle
@@ -27,13 +33,26 @@ particle and move it away from its (potentially and probably) local optimum, to
 improve the ability to find a global optimum. Of course, this comes a the cost
 of slower convergence, but hopefully converges to the global optimum as a result.
 
+If `parallel = true` is specified, there should be a 2-argument method for the objective
+function, `f(val, X)`. The input vectors are columns of `X`. The outputs are written
+into `val`. This makes it possible to parallelize the function evaluations, e.g. with:
+
+```julia
+objfun(x) = sum(x)
+function objfun(val, X)
+    Threads.@threads for i in axes(X, 2)
+        val[i] = objfun(@view(X[:, i]))
+    end
+end
+```
+
 Note, that convergence is never assessed for ParticleSwarm. It will run until it
 reaches the maximum number of iterations set in Optim.Options(iterations=x)`.
 
 ## References
 - [1] Zhan, Zhang, and Chung. Adaptive particle swarm optimization, IEEE Transactions on Systems, Man, and Cybernetics, Part B: CyberneticsVolume 39, Issue 6 (2009): 1362-1381
 """
-ParticleSwarm(; lower = [], upper = [], n_particles = 0) = ParticleSwarm(lower, upper, n_particles)
+ParticleSwarm(; lower = [], upper = [], n_particles = 0, parallel=false) = ParticleSwarm(lower, upper, n_particles, parallel)
 
 Base.summary(::ParticleSwarm) = "Particle Swarm"
 
@@ -93,8 +112,8 @@ function initial_state(method::ParticleSwarm, options, d, initial_x::AbstractArr
     end
 
     @assert length(lower) == length(initial_x) "limits must be of same length as x_initial."
-    @assert all(upper .> lower) "upper must be greater than lower"
-
+    @assert all(upper .>= lower) "upper must be greater than or equal to lower"
+    
     if method.n_particles > 0
         if method.n_particles < 3
           @warn("Number of particles is set to 3 (minimum required)")
@@ -157,8 +176,12 @@ function initial_state(method::ParticleSwarm, options, d, initial_x::AbstractArr
         X_best[j, 1] = initial_x[j]
     end
 
-    for i in 2:n_particles
-        score[i] = value(d, X[:, i])
+    if method.parallel
+        value(d, @view(score[2:n_particles]), @view(X[:, 2:n_particles]))
+    else
+        for i in 2:n_particles
+            score[i] = value(d, X[:, i])
+        end
     end
 
     ParticleSwarmState(
@@ -246,8 +269,12 @@ function update_state!(f, state::ParticleSwarmState{T}, method::ParticleSwarm) w
     if state.limit_search_space
         limit_X!(state.X, state.lower, state.upper, state.n_particles, n)
     end
-    compute_cost!(f, state.n_particles, state.X, state.score)
-
+    if method.parallel
+        compute_cost_parallel!(f, state.n_particles, state.X, state.score)
+    else
+        compute_cost!(f, state.n_particles, state.X, state.score)
+    end
+    
     state.iteration += 1
     false
 end
@@ -480,5 +507,13 @@ function compute_cost!(f,
     for i in 1:n_particles
         score[i] = value(f, X[:, i])
     end
+    nothing
+end
+
+function compute_cost_parallel!(f,
+                       n_particles::Int,
+                       X::Matrix,
+                       score::Vector)
+    value(f, score, X)
     nothing
 end
