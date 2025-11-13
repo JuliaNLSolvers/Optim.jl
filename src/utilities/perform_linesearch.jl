c6f69d65f96edfd4597138f4d223cc6f7ca4564c
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2025-11-13 13:04:09 +0100

    Use gradient!(d, x). in perform_linesearch (#1207)
    * Use gradient!(d, x). in perform_linesearch
    
    This shouldn't matter, but *if* somehow happened to be calling the objective function in the callback or some other way this would now produce the correct result. Previously, it used the most recent evaluation. Now, it will try to evaluate at state.x and if that was the last point evaluated we'll just use the cached result. This should always be the case unless someone messed with the objective outside. We could even assert that g_calls is constant around the call to be sure to catch it but then users wouldn't be able to call the gradient outside at their own expense of runtime.
    
    * Update perform_linesearch.jl

--- src/utilities/perform_linesearch.jl
+++ src/utilities/perform_linesearch.jl
@@ -9,26 +9,27 @@ _alphaguess(a::Number) = LineSearches.InitialStatic(alpha = a)
 # project_tangent! here, because we already did that inplace on gradient(d) after
 # the last evaluation (we basically just always do it)
 function reset_search_direction!(state, d, method::BFGS)
+    gx = gradient!(d, state.x)
     if method.initial_invH === nothing
         n = length(state.x)
         T = typeof(state.invH)
         if method.initial_stepnorm === nothing
             state.invH .= _init_identity_matrix(state.x)
         else
-            initial_scale = method.initial_stepnorm * inv(norm(gradient(d), Inf))
+            initial_scale = method.initial_stepnorm * inv(norm(gx, Inf))
             state.invH .= _init_identity_matrix(state.x, initial_scale)
         end
     else
         state.invH .= method.initial_invH(state.x)
     end
     #    copyto!(state.invH, method.initial_invH(state.x))
-    state.s .= .-gradient(d)
+    state.s .= .-gx
     return true
 end
 
 function reset_search_direction!(state, d, method::LBFGS)
     state.pseudo_iteration = 1
-    state.s .= .-gradient(d)
+    state.s .= .-gradient!(d, state.x)
     return true
 end
 
@@ -39,12 +40,14 @@ end
 
 function perform_linesearch!(state, method, d)
     # Calculate search direction dphi0
-    dphi_0 = real(dot(gradient(d), state.s))
+    fx = value_gradient!(d, state.x)
+    gx = gradient(d)
+    dphi_0 = real(dot(gx, state.s))
     # reset the direction if it becomes corrupted
     if dphi_0 >= zero(dphi_0) && reset_search_direction!(state, d, method)
-        dphi_0 = real(dot(gradient(d), state.s)) # update after direction reset
+        dphi_0 = real(dot(gx, state.s)) # update after direction reset
     end
-    phi_0 = value(d)
+    phi_0 = value!(d, state.x)
 
     # Guess an alpha
     method.alphaguess!(method.linesearch!, state, phi_0, dphi_0, d)
