2359821659a878936a6e049e00708f0a70073cc9
abhro <5664668+abhro@users.noreply.github.com>
2026-04-12 13:30:18 -0400

    Use partial-apply version of `endswith()`

--- docs/generate.jl
+++ docs/generate.jl
@@ -8,7 +8,7 @@ ONLYSTATIC = []
 
 EXAMPLEDIR = joinpath(@__DIR__, "src", "examples")
 GENERATEDDIR = joinpath(@__DIR__, "src", "examples", "generated")
-for example in filter!(x -> endswith(x, ".jl"), readdir(EXAMPLEDIR))
+for example in filter!(endswith(".jl"), readdir(EXAMPLEDIR))
     input = abspath(joinpath(EXAMPLEDIR, example))
     script = Literate.script(input, GENERATEDDIR)
     code = strip(read(script, String))
