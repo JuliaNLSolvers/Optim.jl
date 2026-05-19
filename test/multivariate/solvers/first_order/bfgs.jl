f424e566af990ddbc9226753c1c8689e8a5feca0
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2026-05-19 18:48:03 +0200

    try to run all bfgs

--- test/multivariate/solvers/first_order/bfgs.jl
+++ test/multivariate/solvers/first_order/bfgs.jl
@@ -1,13 +1,6 @@
 @testset "BFGS" begin
-    # Trigonometric gets stuck in a local minimum?
-    # Penalty Function I gives an increase in function value of a runner which is weird given the resets
-    skip = ("Trigonometric", "Penalty Function I")
-
-    iteration_exceptions = (("Extended Powell", 4000),)
     run_optim_tests(
         BFGS();
-        skip,
-        iteration_exceptions,
         show_name = debug_printing,
     )
 end
